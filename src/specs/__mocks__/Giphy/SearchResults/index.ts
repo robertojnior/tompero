22b714248a8b500f9225c408b199532a79c33340
Roberto Júnior <robertojunior085@gmail.com>
2020-10-08 20:16:57 -0300

    Renaming mocks folder to __mocks__

---
+++ src/specs/__mocks__/Giphy/SearchResults/index.ts
@@ -0,0 +1,5 @@
+import { FALLBACK_GIF_URL } from '@utils/constants/GiphyClient'
+
+const giphySearch = { data: { data: [{ images: { fixed_height: { url: FALLBACK_GIF_URL } } }] } }
+
+export default giphySearch
---
+++ src/specs/__mocks__/InvalidHttpStatuses/index.ts
@@ -0,0 +1,7 @@
+const invalidHttpStatuses = [
+  [400, 'Bad Request'],
+  [500, 'Internal Server Error'],
+  [503, 'Service Unavailable']
+]
+
+export default invalidHttpStatuses
